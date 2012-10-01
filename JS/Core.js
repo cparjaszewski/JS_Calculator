91b489841f24d014b4b28203b37ea074930bef5b
krzysztof_parjaszewski <krzysztof.parjaszewski@skygate.pl>
2012-10-01 12:30:58 +0200

    First pack of unit tests

--- JS/Core.js
+++ JS/Core.js
@@ -2,207 +2,262 @@
 * Calculator - Test || Main JS file
 ***********************************/
 
-// Vars
-var bCommaLock = false;
-var sCalculate = "0";
+/**
+ * This calculator adds, substracts, multiplies, divides and takes the square root in pure JS
+ */ 
+calculator = {
+	// Flag to protect before double usage of comma ',' character
+	commaLock : false,
+	
+	// The main calculator expression:
+	currentExpression : "0",
+	
+	// The last typed operand:
+	lastOperand : null,
+	
+	/**
+	* Finds the main calculator's <input> that displays the current calculator state:
+	*/
+	updateDisplay : function() {
+		$('input[name=calc_result]').val(this.currentExpression)
+	},
+	
+	/** 
+	 * Gets the last character of the current calculator expression.
+	 * If the currect expression if empty, it returns 0.
+	 * 
+	 */
+	getLastExpressionCharacter : function() {
+		if (this.currentExpression == '') {
+			return 0;
+		} else {
+			return this.currentExpression.charAt(this.currentExpression.length - 1);
+		}
+	},
+	
+	/** 
+	 * Performs the calculation based on the current expression
+	 */
+	calculate : function() {
+		// We are waiting for the first operand to be entered
+		if (this.lastOperand == null) {
+			return false;
+		}
+	
+		// Get last char from string
+		var lastCharacter = this.getLastExpressionCharacter();
+		
+		// If the last typed characted was a operand - we are still waiting for some number
+		if (lastCharacter == this.lastOperand ) {
+			return false; 
+		} 
+		
+		// The calculator expression evaluation:
+		result = eval(this.currentExpression)
+		
+		// The answer
+		return result.toString();
 
+	},
+	
+	/** 
+	 * Clears the calculator memory and sets 0 to the display
+	 */ 
+	clear: function() {
+		this.currentExpression = "0";
+		this.commaLock = false;
+		this.updateDisplay();
+	}, 
+	
+	/** 
+	 * Starts the calculator
+	 */
+	start : function() {
+		// Self object to remember the calculator reference for event handlers
+		var _this = this;
+		
+		$("button").unbind('click');
+		
+		// Pass click event to click handler
+		$("button").click(function() {
+			_this.clickEventHandler(this, _this);
+		});
+	
+		this.clear(); // Reset calc
 
-// Update display
-function func_DisplayUpdate()
-{
-	$('[name=calc_result]').val(sCalculate);
+		return true;
+	},
+	
+	/**
+	 * Event handler for the 'click' event
+	 *
+	 * param button - the object that is calling the event
+	 */
+	clickEventHandler : function(button, calc) {
+		// Reads the caller id attribute
+		var callerId = button.id;
+		
+		// The id attribute has nto been defined
+		if (callerId == null) {
+			return false;	
+		}
+		
+		var expr = calc.currentExpression;
+		
+		switch(callerId)
+		{
+			// Clear
+			case "id_C":
+				calc.clear();
+			break;
+			
+			// Power of two
+			case "id_Power2":
+				calc.calculate();
+				expr *= expr;
+				expr = expr.toString();
+				calc.updateDisplay();
+			break;
+			
+			// nth root
+			case "id_Radic":
+				calculator.calculate();
+				expr = Math.sqrt(expr);
+				expr = expr.toString();
+				calc.updateDisplay();
+			break;
+			
+			// Percent
+			case "id_Percent":
+				calc.addExpression("%");
+			break;
+			
+			// Divide
+			case "id_Divide":
+				calc.addExpression("/");
+			break;
+			
+			// Multilate
+			case "id_Multi":
+				calc.addExpression("*");
+			break;
+			
+			// Subtract
+			case "id_Subt":
+				calc.addExpression("-");
+			break;
+			
+			// Comma
+			case "id_Comma":
+				calc.addExpression(".");
+			break;
+			
+			// Addition
+			case "id_Add":
+				calc.addExpression("+");
+			break;
+			
+			// Result
+			case "id_Result":
+				calculator.calculate();
+				calculator.updateDisplay();
+			break;
+			
+			// Digits
+			case "id_0":
+			case "id_1":
+			case "id_2":
+			case "id_3":
+			case "id_4":
+			case "id_5":
+			case "id_6":
+			case "id_7":
+			case "id_8":
+			case "id_9":
+				buttonDigit = callerId.replace("id_", ""); // Let's just cut 'id_' from string...
+				calc.addExpression(buttonDigit);
+				expr = calc.currentExpression;
+			break;
+		}
+		
+		calc.currentExpression = expr;
+	},
+	
+	/**
+	 * Appends the clicked expression to the main calculator expression
+	 *
+	 * param expression - expression to add	 
+	 */
+	addExpression : function(expression)  {
+		// Get last char from string
+		var vLastChar = this.getLastExpressionCharacter();
+		
+		// Check vADD
+		switch(expression)
+    {
+			// If NEW char is a digit
+			case "0":
+			case "1":
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
+				if(this.currentExpression == "0") {
+					this.currentExpression = "";
+				}
+				this.currentExpression = this.currentExpression + expression;
+			break;
+			
+			// If NEW char IS NOT a digit
+			case "+":
+			case "-":
+			case "*":
+			case "/":
+			case "%":
+				// Don't add char if there is only 0 digit
+				if(this.currentExpression == "0")
+					break;
+				
+				// If new char == last char, break it, we don't want to waste RES for ( X -> X ) replacement
+				if(vLastChar == expression)
+					break;
+					
+				// If last char is also a math char, then replace it with new one
+				if(vLastChar == "+" || vLastChar == "-" || vLastChar == "*" || vLastChar == "/" || vLastChar == "%")
+					this.currentExpression = this.currentExpression.substr(0, this.currentExpression.length - 1); // Delete last char in string
+					
+				// If last char is comma then add 0 before math symbol
+				if(vLastChar == ".")
+					expression = "0" + expression;
+
+				this.currentExpression = this.currentExpression + expression;
+				this.commaLock = false; // Another math char was passed, so dissable comma lock
+			break;
+			
+			case ".":
+				// Blocks comma after math symbol
+				if(vLastChar == "+" || vLastChar == "-" || vLastChar == "*" || vLastChar == "/" || vLastChar == "%")
+					break;
+			
+				// If comma lock is disabled
+				if(bCommaLock == false)
+				{
+					this.currentExpression = this.currentExpression + expression;
+					this.commaLock = true;
+				}
+			break;
+		}
+
+		// Finally the display is being updated
+		this.updateDisplay();
+		
+		return true; // YAY !
+	},
+	
+	
 };
 
-// Get last char
-function func_GetLastChar(vString)
-{
-	if(vString == "")
-		return "0";
-	else
-		return vString.substr(vString.length - 1);
-}
-
-// Calculate
-function func_DoMath()
-{
-	// Get last char from string
-	var vLastChar = func_GetLastChar(sCalculate); 
-	
-	// If last symbol is math char, that means it's incomplete, ignore math then
-	if(vLastChar == "+" || vLastChar == "-" || vLastChar == "*" || vLastChar == "/" || vLastChar == "%")
-		return false;
-	
-	// Eval is not really safe, but AFAIK it's ok for that kind of usage
-	sCalculate = (eval(sCalculate)).toString(); 
-}
-
-// Append to calculation string
-function func_CalcAppend(vAdd)
-{
-	// Get last char from string
-	var vLastChar = func_GetLastChar(sCalculate); 
-	
-	// Check vADD
-	switch(vAdd)
-	{
-		// If NEW char is a digit
-		case "0":
-		case "1":
-		case "2":
-		case "3":
-		case "4":
-		case "5":
-		case "6":
-		case "7":
-		case "8":
-		case "9":
-			if(sCalculate == "0")
-				sCalculate = "";
-			sCalculate = sCalculate + vAdd;
-		break;
-		
-		// If NEW char IS NOT a digit
-		case "+":
-		case "-":
-		case "*":
-		case "/":
-		case "%":
-			// Don't add char if there is only 0 digit
-			if(sCalculate == "0")
-				break;
-			
-			// If new char == last char, break it, we don't want to waste RES for ( X -> X ) replacement
-			if(vLastChar == vAdd)
-				break;
-				
-			// If last char is also a math char, then replace it with new one
-			if(vLastChar == "+" || vLastChar == "-" || vLastChar == "*" || vLastChar == "/" || vLastChar == "%")
-				sCalculate = sCalculate.substr(0, sCalculate.length - 1); // Delete last char in string
-				
-			// If last char is comma then add 0 before math symbol
-			if(vLastChar == ".")
-				vAdd = "0" + vAdd;
-
-			sCalculate = sCalculate + vAdd;
-			bCommaLock = false; // Another math char was passed, so dissable comma lock
-		break;
-		
-		case ".":
-			// Block comma after math symbol
-			if(vLastChar == "+" || vLastChar == "-" || vLastChar == "*" || vLastChar == "/" || vLastChar == "%")
-				break;
-		
-			// If comma lock is disabled
-			if(bCommaLock == false)
-			{
-				sCalculate = sCalculate + vAdd;
-				bCommaLock = true;
-			}
-		break;
-	}
-
-	func_DisplayUpdate();
-	return true; // YAY !
-}
-
-// Clear
-function func_Clear()
-{
-	sCalculate = "0";
-	bCommaLock = false;
-	func_DisplayUpdate();
-}
-
-// Button click handler
-function func_ExecButtonAction(vBtnID)
-{
-	switch(vBtnID)
-	{
-		// Clear
-		case "id_C":
-			func_Clear();
-		break;
-		
-		// Power of two
-		case "id_Power2":
-			func_DoMath();
-			sCalculate = sCalculate * sCalculate;
-			sCalculate = sCalculate.toString();
-			func_DisplayUpdate();
-		break;
-		
-		// nth root
-		case "id_Radic":
-			func_DoMath();
-			sCalculate = Math.sqrt(sCalculate);
-			sCalculate = sCalculate.toString();
-			func_DisplayUpdate();
-		break;
-		
-		// Percent
-		case "id_Percent":
-			func_CalcAppend("%");
-		break;
-		
-		// Divide
-		case "id_Divide":
-			func_CalcAppend("/");
-		break;
-		
-		// Multilate
-		case "id_Multi":
-			func_CalcAppend("*");
-		break;
-		
-		// Subtract
-		case "id_Subt":
-			func_CalcAppend("-");
-		break;
-		
-		// Comma
-		case "id_Comma":
-			func_CalcAppend(".");
-		break;
-		
-		// Addition
-		case "id_Add":
-			func_CalcAppend("+");
-		break;
-		
-		// Result
-		case "id_Result":
-			func_DoMath();
-			func_DisplayUpdate();
-		break;
-		
-		// Digits
-		case "id_0":
-		case "id_1":
-		case "id_2":
-		case "id_3":
-		case "id_4":
-		case "id_5":
-		case "id_6":
-		case "id_7":
-		case "id_8":
-		case "id_9":
-			vBtnID = vBtnID.replace("id_", ""); // Let's just cut 'id_' from string...
-			func_CalcAppend(vBtnID);
-		break;
-	}
-}
-
-
-// Initialize
-function func_ExecMain()
-{
-	// Pass click event to click handler
-	$("button").click(function()
-	{
-		func_ExecButtonAction(this.id);
-	});
-	
-	func_Clear(); // Reset calc
-
-	return true;
-}
--- JS/Test.js
+++ JS/Test.js
@@ -2,4 +2,87 @@
 * Tester
 *********/
 
+
 // Place for some tests
+module("Core.js tests", {
+	buttonArray : [
+			'<button id="id_C" class="c_calc_button" style="color: #bb0000">C</button>',
+			'<button id="id_Power2" class="c_calc_button" style="color: #004ea5">x^2</button>',
+			'<button id="id_Radic" class="c_calc_button" style="color: #004ea5">&radic;</button>',
+			'<button id="id_Percent" class="c_calc_button" style="color: #004ea5">mod</button>',
+			'<button id="id_7" class="c_calc_button">7</button>',
+			'<button id="id_8" class="c_calc_button">8</button>',
+			'<button id="id_9" class="c_calc_button">9</button>',
+			'<button id="id_Divide" class="c_calc_button" style="color: #004ea5">/</button>',
+			'<button id="id_4" class="c_calc_button">4</button>',
+			'<button id="id_5" class="c_calc_button">5</button>',
+			'<button id="id_6" class="c_calc_button">6</button>',
+			'<button id="id_Multi" class="c_calc_button" style="color: #004ea5">*</button>',
+			'<button id="id_1" class="c_calc_button">1</button>',
+			'<button id="id_2" class="c_calc_button">2</button>',
+			'<button id="id_3" class="c_calc_button">3</button>',
+			'<button id="id_Subt" class="c_calc_button" style="color: #004ea5">-</button>',
+			'<button id="id_0" class="c_calc_button">0</button>',
+			'<button id="id_Comma" class="c_calc_button" style="color: #004ea5">,</button>',
+			'<button id="id_Add" class="c_calc_button" style="color: #004ea5">+</button>',
+			'<button id="id_Result" class="c_calc_button" style="color: #00a317">=</button>'
+	],
+	setup: function() {
+		ok(true, "setup() has been launched");		
+		$('body').after('<div id="test-panel-data" style="display:none;">'+this.buttonArray+'</div>');
+	},
+	teardown: function() {
+		ok(true, "teardown() has been launched");
+		$('#test-panel-data').remove();
+	},
+	
+});
+
+test("Init environment check", function() {
+	ok(null != calculator , "calculator must be defined, otherwise tests do not have any sensd");
+});
+
+
+test("start() method assertions", function() {
+	calculator.start();
+	$(this.buttonArray).each(function(button) {
+		var button = $(this+"");
+		if ( 
+				(null != (obj = document.getElementById(button.attr("id"))) ) &&
+				(null != (events = jQuery._data(obj).events) ) &&
+				(null != (clickEventArray = events.click))
+			) 
+		{
+			ok(clickEventArray.length == 1, "We must have one and only one click event!");
+		} else {
+			ok(false, "Error occured - the giver buttonArray is not valid ");
+		}
+		ok(null != clickEventArray[0].handler, "determines if the button is already bound with the proper eventHandler");
+	});
+});
+
+test("clickEventHandler() method assertions", function() {
+	
+	ok(false == calculator.clickEventHandler('<button></button>', calculator), "for empty id the clickEventHandler function must return false");
+	buttonId1 = document.createElement('button');
+	buttonId1.setAttribute("id","id_1");
+	calculator.clickEventHandler(buttonId1, calculator);
+	ok("1" == calculator.currentExpression, "after clicking 1, the calculator's current expression must be updated");
+	
+});
+
+test("updateDisplay() method aasertions", function() {
+
+});
+test("getLastExpressionCharacter() method aasertions", function() {
+
+});
+test("calculate() method aasertions", function() {
+
+});
+test("clear() method aasertions", function() {
+
+});
+test("addExpression() method aasertions", function() {
+
+});
